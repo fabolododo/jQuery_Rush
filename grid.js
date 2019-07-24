15946659f412e54924adac27bd01aef08d6f92d7
fabolododo <fabolododo@hotmail.fr>
2019-07-24 16:30:51 +0200

    added css grid-container & grid-cell

--- grid.js
+++ grid.js
@@ -1,12 +1,17 @@
 (function ($){
     $.fn.gridBuild = function(){
         for(var rows = 0;rows < 4; rows++){
+
             for(var columns = 0; columns < 4; columns++){
                 $("#grid-container").append('<div class ="grid" id="'+rows+'-'+columns+'">0</div>');
             };
             
         };
-        $(".grid").width(550/4);
-        $(".grid").height(550/4);
+        $(".grid").width(400/4);
+        $(".grid").height(400/4);
+    }
+
+    $.fn.initTule = function(){
+
     }
 })(jQuery);
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $("body").gridBuild();
+    $("#grid-container").gridBuild();
 })
